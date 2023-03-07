129922ae7589f0b48df8a96ea59e6875545c4090
acarriedev <acarrie.dev@gmail.com>
2023-03-07 15:11:36 +0000

    APM-3713 Refactor extended attributes

--- tests/utils/config.py
+++ tests/utils/config.py
@@ -5,7 +5,8 @@ from os import getenv
 ENV = {
     "proxy_name": getenv("PROXY_NAME"),
     "api_name": getenv("API_NAME", "shared-flow-testing"),
-    "source_commit_id": getenv("SOURCE_COMMIT_ID")
+    "source_commit_id": getenv("SOURCE_COMMIT_ID"),
+    "oauth_base_uri": getenv("OAUTH_BASE_URI"),
 }
 
 # OLD CONFIG
---
+++ tests/utils/helpers.py
@@ -0,0 +1,10 @@
+import json
+
+
+def get_variable_from_trace(debug, session_name, variable):
+    trace_ids = debug.get_transaction_data(session_name=session_name)
+    trace_data = debug.get_transaction_data_by_id(
+        session_name=session_name, transaction_id=trace_ids[0]
+    )
+
+    return debug.get_apigee_variable_from_trace(name=variable, data=trace_data)
