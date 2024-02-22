da70d9960e5b8497073bb89c750283be070ecf71
Jack Spagnoli <Jack.spagnoli1@nhs.net>
2024-02-22 09:38:12 +0000

    refactors test params

--- tests/test_user_role_service_v2.py
+++ tests/test_user_role_service_v2.py
@@ -1,6 +1,182 @@
 import pytest
 import requests
 
+HAPPY_PATH_PARAMS = [
+    pytest.param(
+        {},
+        "555254242105",
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750107"},
+            force_new_token=True,
+        ),
+        id="User role sent in id token",
+    ),
+    pytest.param(
+        {},
+        "555254242105",
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "787807429512"},
+            force_new_token=True,
+        ),
+        id="User role in user info - one role - (no header, not in id token)",
+    ),
+    pytest.param(
+        {"NHSD-Session-URID": "656014452101"},
+        "656014452101",
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750104"},
+            force_new_token=True,
+        ),
+        id="User role sent in header (no in id token, multiple in user info)",
+    ),
+]
+UNHAPPY_PATH_PARAMS = [
+    pytest.param(
+        {},
+        "multiple roles found in user info, please check NHSD-Session-URID",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750104"},
+            force_new_token=True,
+        ),
+        id="Multiple user roles found in userinfo",
+    ),
+    pytest.param(
+        {},
+        "no userroles available, please check NHSD-Session-URID is valid",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "Aal3"},
+            force_new_token=True,
+        ),
+        id="No user role provided by any means",
+    ),
+    pytest.param(
+        {},
+        "selected_roleid is misconfigured/invalid",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "Aal4"},
+            force_new_token=True,
+        ),
+        id="User role sent in id token but empty",
+    ),
+    pytest.param(
+        {},
+        "nhsid_nrbac_roles is misconfigured/invalid",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "Aal5"},
+            force_new_token=True,
+        ),
+        id="nrbac is malformed, or person_roleid is empty (in userinfo)",
+    ),
+    pytest.param(
+        {"NHSD-Session-URID": "notAuserRole123"},
+        "NHSD-Session-URID is invalid",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750104"},
+            force_new_token=True,
+        ),
+        id="Invalid role in header",
+    ),
+]
+WHEN_NOT_CIS2_PARAMS = [
+    pytest.param(
+        {},
+        "selected_roleid is missing in your token",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="patient",
+            level="P9",
+            login_form={"username": "9912003071"},
+            force_new_token=True,
+        ),
+        id="NHS Login combined: Role can't be used from token",
+    ),
+    pytest.param(
+        {"NHSD-Session-URID": "9912003071"},
+        "unable to retrieve user info",
+        500,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="patient",
+            level="P9",
+            login_form={"username": "9912003071"},
+            force_new_token=True,
+        ),
+        id="NHS Login combined: Can't use header to fetch from userinfo",
+    ),
+    pytest.param(
+        {},
+        "selected_roleid is missing in your token",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="patient",
+            level="P9",
+            login_form={"username": "9912003071"},
+            authentication="separate",
+            force_new_token=True,
+        ),
+        id="NHS Login separate: Role can't be used from token",
+    ),
+    pytest.param(
+        {"NHSD-Session-URID": "9912003071"},
+        "unable to retrieve user info",
+        500,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="patient",
+            level="P9",
+            login_form={"username": "9912003071"},
+            authentication="separate",
+            force_new_token=True,
+        ),
+        id="NHS Login separate: Can't use header to fetch from userinfo",
+    ),
+    pytest.param(
+        {},
+        "selected_roleid is missing in your token",
+        401,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750104"},
+            authentication="separate",
+            force_new_token=True,
+        ),
+        id="CIS2 separate: Role can't be used from token",
+    ),
+    pytest.param(
+        {"NHSD-Session-URID": "656005750104"},
+        "unable to retrieve user info",
+        500,
+        marks=pytest.mark.nhsd_apim_authorization(
+            access="healthcare_worker",
+            level="aal3",
+            login_form={"username": "656005750104"},
+            authentication="separate",
+            force_new_token=True,
+        ),
+        id="CIS2 separate: Can't use header to fetch from userinfo",
+    ),
+]
+
 
 def set_custom_header_name(headers):
     default_header = headers.get("NHSD-Session-URID")
@@ -10,183 +186,7 @@ def set_custom_header_name(headers):
 class TestUserRoles:
     """A test suite for testing userrole in id tokens/headers/userinfo"""
 
-    happy_path_params = [
-        pytest.param(
-            {},
-            "555254242105",
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750107"},
-                force_new_token=True,
-            ),
-            id="User role sent in id token",
-        ),
-        pytest.param(
-            {},
-            "555254242105",
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "787807429512"},
-                force_new_token=True,
-            ),
-            id="User role in user info - one role - (no header, not in id token)",
-        ),
-        pytest.param(
-            {"NHSD-Session-URID": "656014452101"},
-            "656014452101",
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750104"},
-                force_new_token=True,
-            ),
-            id="User role sent in header (no in id token, multiple in user info)",
-        ),
-    ]
-    unhappy_path_params = [
-        pytest.param(
-            {},
-            "multiple roles found in user info, please check NHSD-Session-URID",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750104"},
-                force_new_token=True,
-            ),
-            id="Multiple user roles found in userinfo",
-        ),
-        pytest.param(
-            {},
-            "no userroles available, please check NHSD-Session-URID is valid",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "Aal3"},
-                force_new_token=True,
-            ),
-            id="No user role provided by any means",
-        ),
-        pytest.param(
-            {},
-            "selected_roleid is misconfigured/invalid",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "Aal4"},
-                force_new_token=True,
-            ),
-            id="User role sent in id token but empty",
-        ),
-        pytest.param(
-            {},
-            "nhsid_nrbac_roles is misconfigured/invalid",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "Aal5"},
-                force_new_token=True,
-            ),
-            id="nrbac is malformed, or person_roleid is empty (in userinfo)",
-        ),
-        pytest.param(
-            {"NHSD-Session-URID": "notAuserRole123"},
-            "NHSD-Session-URID is invalid",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750104"},
-                force_new_token=True,
-            ),
-            id="Invalid role in header",
-        ),
-    ]
-    when_not_cis2_params = [
-        pytest.param(
-            {},
-            "selected_roleid is missing in your token",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="patient",
-                level="P9",
-                login_form={"username": "9912003071"},
-                force_new_token=True,
-            ),
-            id="NHS Login combined: Role can't be used from token",
-        ),
-        pytest.param(
-            {"NHSD-Session-URID": "9912003071"},
-            "unable to retrieve user info",
-            500,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="patient",
-                level="P9",
-                login_form={"username": "9912003071"},
-                force_new_token=True,
-            ),
-            id="NHS Login combined: Can't use header to fetch from userinfo",
-        ),
-        pytest.param(
-            {},
-            "selected_roleid is missing in your token",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="patient",
-                level="P9",
-                login_form={"username": "9912003071"},
-                authentication="separate",
-                force_new_token=True,
-            ),
-            id="NHS Login separate: Role can't be used from token",
-        ),
-        pytest.param(
-            {"NHSD-Session-URID": "9912003071"},
-            "unable to retrieve user info",
-            500,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="patient",
-                level="P9",
-                login_form={"username": "9912003071"},
-                authentication="separate",
-                force_new_token=True,
-            ),
-            id="NHS Login separate: Can't use header to fetch from userinfo",
-        ),
-        pytest.param(
-            {},
-            "selected_roleid is missing in your token",
-            401,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750104"},
-                authentication="separate",
-                force_new_token=True,
-            ),
-            id="CIS2 separate: Role can't be used from token",
-        ),
-        pytest.param(
-            {"NHSD-Session-URID": "656005750104"},
-            "unable to retrieve user info",
-            500,
-            marks=pytest.mark.nhsd_apim_authorization(
-                access="healthcare_worker",
-                level="aal3",
-                login_form={"username": "656005750104"},
-                authentication="separate",
-                force_new_token=True,
-            ),
-            id="CIS2 separate: Can't use header to fetch from userinfo",
-        ),
-    ]
-
-    @pytest.mark.parametrize("additional_headers,expected_urid", happy_path_params)
+    @pytest.mark.parametrize("additional_headers,expected_urid", HAPPY_PATH_PARAMS)
     def test_user_role_happy_path_default_header(
         self,
         nhsd_apim_proxy_url,
@@ -202,7 +202,7 @@ class TestUserRoles:
         assert resp.status_code == 200
         assert resp.headers["NHSD-Session-URID"] == expected_urid
 
-    @pytest.mark.parametrize("additional_headers,expected_urid", happy_path_params)
+    @pytest.mark.parametrize("additional_headers,expected_urid", HAPPY_PATH_PARAMS)
     def test_user_role_happy_path_custom_header(
         self,
         nhsd_apim_proxy_url,
@@ -222,7 +222,7 @@ class TestUserRoles:
         assert resp.headers["NHSD-URID"] == expected_urid
 
     @pytest.mark.parametrize(
-        "additional_headers,error_description,status_code", unhappy_path_params
+        "additional_headers,error_description,status_code", UNHAPPY_PATH_PARAMS
     )
     def test_user_role_unhappy_path_default_header(
         self,
@@ -241,7 +241,7 @@ class TestUserRoles:
         assert resp.json()["issue"][0]["diagnostics"] == error_description
 
     @pytest.mark.parametrize(
-        "additional_headers,error_description,status_code", unhappy_path_params
+        "additional_headers,error_description,status_code", UNHAPPY_PATH_PARAMS
     )
     def test_user_role_unhappy_path_custom_header(
         self,
@@ -264,7 +264,7 @@ class TestUserRoles:
         assert resp.json()["issue"][0]["diagnostics"] == error_description
 
     @pytest.mark.parametrize(
-        "additional_headers,error_description,status_code", when_not_cis2_params
+        "additional_headers,error_description,status_code", WHEN_NOT_CIS2_PARAMS
     )
     def test_error_when_not_cis2_combined_auth_default_header(
         self,
@@ -283,7 +283,7 @@ class TestUserRoles:
         assert resp.json()["issue"][0]["diagnostics"] == error_description
 
     @pytest.mark.parametrize(
-        "additional_headers,error_description,status_code", when_not_cis2_params
+        "additional_headers,error_description,status_code", WHEN_NOT_CIS2_PARAMS
     )
     def test_error_when_not_cis2_combined_auth_custom_header(
         self,
