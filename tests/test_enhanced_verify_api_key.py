ea646f1ef40ff06b1c3c3a32c557999df23b241d
Fayez Ahmed <fayez.ahmed1@nhs.net>
2024-04-24 12:49:24 +0100

    APM-5256: Add tests

--- tests/test_enhanced_verify_api_key.py
+++ tests/test_enhanced_verify_api_key.py
@@ -3,178 +3,76 @@ import pytest
 import requests
 
 
-class TestEnhancedVerifyApiKey():
-    """Test enhanced verify api key policy"""
+@pytest.fixture
+def create_test_app(
+    _create_function_scoped_test_app,
+    _proxy_product_with_scope,
+    developer_apps_api,
+    nhsd_apim_config,
+):
+    """Fixture for creating test app"""
+    app = _create_function_scoped_test_app
+    app = developer_apps_api.put_app_by_name(
+        email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
+    )
+    return app
 
-    @pytest
-    def test_valid_api_key_products_subscribed(
-        self,
-        nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        _proxy_product_with_scope,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-    ):
 
-        # Update test app with proxy product and the extended attribute
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = [_proxy_product_with_scope["name"]]
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
+@pytest.fixture
+def tracing(trace):
+    """Fixture for tracing setup and teardown"""
 
-        # Trace call to shared flow proxy enhanced verify apikey endpoint
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
+    session_name = str(uuid4())
+    header_filters = {"trace_id": session_name}
+    trace.post_debugsession(session=session_name, header_filters=header_filters)
+    yield session_name, header_filters
+    trace.delete_debugsession_by_name(session_name)
 
-        apikey = app["credentials"][0]["consumerKey"],
 
-        proxy_resp = requests.get(
-            url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
-            headers={"apikey": apikey, **header_filters},
-        )
-        assert proxy_resp.status_code == 200
+@pytest.mark.parametrize("apiProducts, expected_status", [
+    (["_proxy_product_with_scope['name']"], 200),
+    ([], 403)
+])
+def test_valid_api_key(nhsd_apim_proxy_url, create_test_app, tracing,
+                       _proxy_product_with_scope, apiProducts, expected_status):
+    session_name, header_filters = tracing
+    apikey = create_test_app["credentials"][0]["consumerKey"]
+    create_test_app["apiProducts"] = apiProducts
 
-        trace.delete_debugsession_by_name(session_name)
+    proxy_resp = requests.get(
+        url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
+        headers={"apikey": apikey, **header_filters},
+    )
 
-    @pytest
-    def test_valid_api_key_no_products_subscribed(
-        self,
-        nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-    ):
+    assert proxy_resp.status_code == expected_status
 
-        # Update test app with proxy product and the extended attribute
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = []
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
 
-        # Trace call to shared flow proxy enhanced verify apikey endpoint
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
+@pytest.mark.parametrize("apiProducts", [
+    ["_proxy_product_with_scope['name']"],
+    []
+])
+def test_revoked_api_key(nhsd_apim_proxy_url, create_test_app, tracing,
+                         _proxy_product_with_scope, apiProducts):
+    session_name, header_filters = tracing
+    apikey = create_test_app["credentials"][0]["consumerKey"]
+    create_test_app["apiProducts"] = apiProducts
+    create_test_app["credentials"][0]["status"] = "revoked"
 
-        apikey = app["credentials"][0]["consumerKey"],
+    proxy_resp = requests.get(
+        url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
+        headers={"apikey": apikey, **header_filters},
+    )
 
-        proxy_resp = requests.get(
-            url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
-            headers={"apikey": apikey, **header_filters},
-        )
-        assert proxy_resp.status_code == 403
+    assert proxy_resp.status_code == 401
 
-        trace.delete_debugsession_by_name(session_name)
 
-    @pytest
-    def test_revoked_api_key_products_subscribed(
-        self,
-        nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        _proxy_product_with_scope,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-    ):
+def test_invalid_api_key(nhsd_apim_proxy_url, create_test_app, tracing):
+    session_name, header_filters = tracing
+    apikey = "invalidapikey123"
 
-        # Update test app with proxy product and the extended attribute
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = [_proxy_product_with_scope["name"]]
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
+    proxy_resp = requests.get(
+        url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
+        headers={"apikey": apikey, **header_filters},
+    )
 
-        # Trace call to shared flow proxy enhanced verify apikey endpoint
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
-
-        app["credentials"][0]["status"] = "revoked"
-        apikey = app["credentials"][0]["consumerKey"],
-
-        proxy_resp = requests.get(
-            url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
-            headers={"apikey": apikey, **header_filters},
-        )
-        assert proxy_resp.status_code == 401
-
-        trace.delete_debugsession_by_name(session_name)
-
-    @pytest
-    def test_revoked_api_key_no_products_subscribed(
-        self,
-        nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-    ):
-
-        # Update test app with proxy product and the extended attribute
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = []
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
-
-        # Trace call to shared flow proxy enhanced verify apikey endpoint
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
-
-        app["credentials"][0]["status"] = "revoked"
-        apikey = app["credentials"][0]["consumerKey"],
-
-        proxy_resp = requests.get(
-            url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
-            headers={"apikey": apikey, **header_filters},
-        )
-        assert proxy_resp.status_code == 401
-
-        trace.delete_debugsession_by_name(session_name)
-
-    @pytest
-    def test_invalid_api_key(
-        self,
-        nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-    ):
-
-        # Update test app with proxy product and the extended attribute
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = []
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
-
-        # Trace call to shared flow proxy enhanced verify apikey endpoint
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
-
-        apikey = "invalidapikey123"
-        proxy_resp = requests.get(
-            url=f"{nhsd_apim_proxy_url}/enhanced-verify-api-key",
-            headers={"apikey": apikey, **header_filters},
-        )
-        assert proxy_resp.status_code == 401
-
-        trace.delete_debugsession_by_name(session_name)
+    assert proxy_resp.status_code == 401
