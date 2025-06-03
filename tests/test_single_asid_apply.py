d13b24e36875062f9cc7a47a682fb6d8cc16b7ab
NautiyalVikas1 <157499123+NautiyalVikas1@users.noreply.github.com>
2025-06-03 10:33:57 +0100

    APM-6193-Test Case

--- tests/test_single_asid_apply.py
+++ tests/test_single_asid_apply.py
@@ -40,52 +40,21 @@ class TestSingleAsidApply:
         assert token_resp.status_code == 200
 
         return token_resp.json()["access_token"]
-
-    
-    def test_single_asid_apply(
+    @pytest.mark.parametrize("expected_status_code, expected_message", [(400, "no_headers")])
+    def test_no_header_present(
         self,
         nhsd_apim_proxy_url,
-        _create_function_scoped_test_app,
-        _proxy_product_with_scope,
-        _jwt_keys,
-        developer_apps_api,
-        nhsd_apim_config,
-        trace,
-        new_attribute,
-        flow_vars_to_check,
+        expected_status_code,
+        expected_message
     ):
-        # Update test app with proxy product and the single asid
-        app = _create_function_scoped_test_app
-        app["apiProducts"] = [_proxy_product_with_scope["name"]]
-        app["attributes"].append(new_attribute)
-        app = developer_apps_api.put_app_by_name(
-            email=nhsd_apim_config["APIGEE_DEVELOPER"], app_name=app["name"], body=app
-        )
+        # Create test app and don't define any apiProducts
 
-        # Trace call to shared flow proxy single asid
-        session_name = str(uuid4())
-        header_filters = {"trace_id": session_name}
-        trace.post_debugsession(session=session_name, header_filters=header_filters)
-
-        access_token = self._get_token_client_credentials(
-            client_id=app["credentials"][0]["consumerKey"],
-            private_key=_jwt_keys["private_key_pem"],
-        )
         proxy_resp = requests.get(
             url=f"{nhsd_apim_proxy_url}/single-asid",
-            headers={"Authorization": f"Bearer {access_token}","NHSD-End-User-Organisation-ODS": "X09001", **header_filters},
+            timeout=60
         )
-        assert proxy_resp.status_code == 200
 
-        # Extract variables from trace and assert
-        extended_attributes = get_variable_from_trace(
-            trace, session_name, "app." + new_attribute["name"]
-        )
-        assert extended_attributes == new_attribute["value"]
+        assert proxy_resp.status_code == expected_status_code
+        assert proxy_resp.json().get("error") == expected_message
+    
 
-        for flow_var in flow_vars_to_check:
-            assert flow_var["value"] == get_variable_from_trace(
-                trace, session_name, flow_var["name"]
-            )
-
-        trace.delete_debugsession_by_name(session_name)
